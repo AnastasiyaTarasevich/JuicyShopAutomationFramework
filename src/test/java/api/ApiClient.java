c7b038caef26a54cf99c0b31387704324b49080f
Nastya <at9183002@gmail.com>
2025-12-15 13:33:09 +0300

    removed RP listener from local

--- src/test/java/api/ApiClient.java
+++ src/test/java/api/ApiClient.java
@@ -4,109 +4,84 @@ import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import lombok.AllArgsConstructor;
 import static io.restassured.RestAssured.given;
 
-@AllArgsConstructor
 public class ApiClient {
 
-    public <T> Response post(String path, T body) {
-        RequestSpecification request = given()
-                .filter(new AllureRestAssured())
-                .baseUri(RestAssured.baseURI)
-                .contentType("application/json");
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String BEARER = "Bearer ";
+    private static final String JSON = "application/json";
 
-        if (body != null) {
-            request.body(body);
-        }
-
-        return request
-                .when()
-                .post(path)
-                .then()
-                .log().ifError()
-                .extract().response();
-    }
-
-    public <T> Response postWithAuth(String path, T body, String token) {
-        RequestSpecification request = given()
-                .filter(new AllureRestAssured())
-                .baseUri(RestAssured.baseURI)
-                .contentType("application/json")
-                .header("Authorization", "Bearer " + token);
-
-        if (body != null) {
-            request.body(body);
-        }
-
-        return request
-                .when()
-                .post(path)
-                .then()
-                .log().ifError()
-                .extract().response();
-    }
-
-    public Response get(String path) {
+    private RequestSpecification baseRequest() {
         return given()
                 .filter(new AllureRestAssured())
                 .baseUri(RestAssured.baseURI)
-                .when()
-                .get(path)
-                .then()
-                .log().ifError()
-                .extract().response();
+                .contentType(JSON);
     }
 
-    public Response getWithAuth(String path, String token) {
-        return given()
-                .filter(new AllureRestAssured())
-                .baseUri(RestAssured.baseURI)
-                .header("Authorization", "Bearer " + token)
-                .when()
-                .get(path)
-                .then()
-                .log().ifError()
-                .extract().response();
+    private RequestSpecification baseRequestWithAuth(String token) {
+        return baseRequest()
+                .header(AUTH_HEADER, BEARER + token);
     }
 
-    public Response getWithQueryParam(String path, String queryParam) {
-        return given()
-                .filter(new AllureRestAssured())
-                .baseUri(RestAssured.baseURI)
-                .queryParam("q", queryParam)
-                .when()
-                .get(path)
-                .then()
-                .log().ifError()
-                .extract().response();
-    }
-
-    public Response getWithPathParamAndAuth(String path, String id, String token) {
-        return given()
-                .filter(new AllureRestAssured())
-                .baseUri(RestAssured.baseURI)
-                .header("Authorization", "Bearer " + token)
-                .pathParam("id", id)
-                .when()
-                .get(path)
-                .then()
-                .log().ifError()
-                .extract().response();
-    }
-
-    public Response delete(String pathTemplate, String token, int pathParam) {
-        return given()
-                .filter(new AllureRestAssured())
-                .header("Authorization", "Bearer " + token)
-                .baseUri(RestAssured.baseURI)
-                .pathParam("basketItemId", pathParam)
-                .when()
-                .delete(pathTemplate)
-                .then()
+    private Response extract(Response response) {
+        return response.then()
                 .log().ifError()
                 .extract()
                 .response();
     }
 
+    // ---------- POST ----------
+
+    public <T> Response post(String path, T body) {
+        RequestSpecification request = baseRequest();
+        if (body != null) {
+            request.body(body);
+        }
+        return extract(request.post(path));
+    }
+
+    public <T> Response postWithAuth(String path, T body, String token) {
+        RequestSpecification request = baseRequestWithAuth(token);
+        if (body != null) {
+            request.body(body);
+        }
+        return extract(request.post(path));
+    }
+
+    // ---------- GET ----------
+
+    public Response get(String path) {
+        return extract(baseRequest().get(path));
+    }
+
+    public Response getWithAuth(String path, String token) {
+        return extract(baseRequestWithAuth(token).get(path));
+    }
+
+    public Response getWithQueryParam(String path, String query) {
+        return extract(
+                baseRequest()
+                        .queryParam("q", query)
+                        .get(path)
+        );
+    }
+
+    public Response getWithPathParamAndAuth(String path, String id, String token) {
+        return extract(
+                baseRequestWithAuth(token)
+                        .pathParam("id", id)
+                        .get(path)
+        );
+    }
+
+    // ---------- DELETE ----------
+
+    public Response delete(String pathTemplate, String token, int basketItemId) {
+        return extract(
+                baseRequestWithAuth(token)
+                        .pathParam("basketItemId", basketItemId)
+                        .delete(pathTemplate)
+        );
+    }
 }
